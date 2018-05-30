3e5906502e1852b5601b680f4f6898edea8dc686
SugaryPoleCat <sugarypolecat@gmail.com>
2018-05-30 13:43:04 +0200

    Revert "added filterList function"
    This reverts commit 2630c78b99a54a2e50b9da7171c7cd7af7c7e030.

--- assets/js/main.js
+++ assets/js/main.js
@@ -214,33 +214,3 @@ jQuery(document).ready(function ($) {
 
     //End
 });
-
-/* TOUCH IT AND YOU DIE!!!! */
-
-
-$(function () {
-		
-	var filterList = {
-	
-		init: function () {
-		
-			// MixItUp plugin
-			// http://mixitup.io
-			$('#portfoliolist').mixItUp({
-				selectors: {
-  			  target: '.portfolio',
-  			  filter: '.filter'	
-  		  },
-  		  load: {
-    		  filter: '.app' // show app tab on first load
-    		}     
-			});								
-		
-		}
-
-	};
-	
-	// Run the show!
-	filterList.init();
-	
-});		
