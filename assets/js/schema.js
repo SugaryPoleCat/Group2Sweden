e479fd0fe3af24afd2d41c44479445213d3af05d
SugaryPoleCat <sugarypolecat@gmail.com>
2018-06-04 13:09:27 +0200

    SCHMEA

--- assets/js/schema.js
+++ assets/js/schema.js
@@ -1,6 +1,6 @@
-/*var schedule = angular.module('osSchema', []);
-schedule.controller('schemaCtrl',function($scope,$http){
-    $http.get("schedule2.json").then(function(response){
-        $scope.myData=response.data;
-    });
-});*/
+var schedule = angular.module('osSchema', []);
+schedule.controller('schemaCtrl',function($scope, $http){
+    $http.get("schedule2.json").then(function (response){
+        $scope.myData = response.data;
+    })
+})
